7464b2c1c7c57b9366efcd6ae7ed95f03e8cf734
ageuxo <ageuxo@gmail.com>
2026-01-09 20:30:16 +0100

    Move creative check to use usePlayerItem() instead

--- common/src/main/java/dev/tophatcat/projecticbp/entities/BallisticPenguinEntity.java
+++ common/src/main/java/dev/tophatcat/projecticbp/entities/BallisticPenguinEntity.java
@@ -46,7 +46,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
-import net.minecraft.world.phys.Vec3;
 import net.tslat.smartbrainlib.api.SmartBrainOwner;
 import net.tslat.smartbrainlib.api.core.BrainActivityGroup;
 import net.tslat.smartbrainlib.api.core.SmartBrainProvider;
